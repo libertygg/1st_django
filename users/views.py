355844ea48208b3d457446f340b3751f079aa097
libertygg <liberty_gg@mail.ru>
2025-07-15 14:30:26 +0300

    Реализовали страничку авторизации через формы Джанго

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import User
+from .forms import UserLoginForm
 
 def login(request):
-    return render(request, 'users/login.html')
+    context = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', context)
 
 def registration(request):
     return render(request, 'users/registration.html')
