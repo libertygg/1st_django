8bf425fb1729233da9e8c5f912b10ffce6066fe9
libertygg <liberty_gg@mail.ru>
2025-07-16 11:22:08 +0300

    Кастомизировали форму авторизации

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,12 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from .models import User
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'plaseholder': 'Введите имя пользователя'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'plaseholder': 'Введите пароль'}))
     class Meta:
         model = User
         fields = ('username', 'password')
