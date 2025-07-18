7b4ff1d16363e1da2b8b288834dff61f78ceb3fd
libertygg <liberty_gg@mail.ru>
2025-07-18 14:32:57 +0300

    Выстроили работу личного кабинета пользователя

--- users/views.py
+++ users/views.py
@@ -30,6 +30,14 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 def profile(request):
-    form = UserProfileForm(instance=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
     context = {'title': 'Store - Профиль', 'form': form}
     return render(request, 'users/profile.html', context)
